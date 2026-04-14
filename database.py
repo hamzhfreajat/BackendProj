c11f9990ae294fb01ffbf55bb2a57b199eb4c5d2
Hamza Fraijat <hfraijat@optimizasolutions.com>
2026-04-14 22:47:41 +0300

    feat: support dynamic db port

--- database.py
+++ database.py
@@ -8,8 +8,9 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
 DB_NAME = os.getenv("DB_NAME", "open")
 # Use localhost to connect to the Windows machine's native postgres instance
 DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
